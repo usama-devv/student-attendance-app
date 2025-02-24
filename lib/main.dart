700997a82c97a76f2b00bed3c35edc63e4a83baf
Usama Masood <134695768+usama-devv@users.noreply.github.com>
2025-02-24 13:10:23 +0500

    firebase integrated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:student_attendance_app/views/home.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -10,7 +14,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Student Attendance',
       theme: ThemeData(
--- lib/views/add_student.dart
+++ lib/views/add_student.dart
@@ -8,16 +8,21 @@ class AddStudent extends StatefulWidget {
 }
 
 class _AddStudentState extends State<AddStudent> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController rollNoController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         margin: EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                Icon(Icons.arrow_back_ios),
+                SizedBox(width: 30.0),
                 Text(
                   "Student ",
                   style: TextStyle(
@@ -35,6 +40,99 @@ class _AddStudentState extends State<AddStudent> {
                   ),
                 ),
               ],
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Student Name",
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.only(left: 20.0),
+              decoration: BoxDecoration(
+                color: Color(0xFFececf8),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: TextField(
+                controller: nameController,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Enter Student Name",
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Student RollNo.",
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.only(left: 20.0),
+              decoration: BoxDecoration(
+                color: Color(0xFFececf8),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: TextField(
+                controller: rollNoController,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Enter Student RollNo.",
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Student Age",
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.only(left: 20.0),
+              decoration: BoxDecoration(
+                color: Color(0xFFececf8),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: TextField(
+                controller: ageController,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Enter Student Age",
+                ),
+              ),
+            ),
+            SizedBox(height: 40.0),
+            Center(
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                width: 150,
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Text(
+                    "Submit",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24.0,
+                    ),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
